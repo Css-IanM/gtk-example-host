ee4957b7cdb433930ce15190b3864f2b2e6bc511
IanM <ian.mackie@kofile.us>
2019-12-05 12:24:22 -0500

    Cleanup and naming

--- src/forms/LoginWindow.cs
+++ src/forms/LoginWindow.cs
@@ -8,37 +8,52 @@ namespace Jupiter
     public class LoginWindow : Window
     {
         #region UI References
-        [UI] private Button _btnLogin = null;
-        [UI] private Button _btnCancel = null;
-        [UI] private Button _btnConfig = null;
+        [UI] private Button btnLogin = null;
+        [UI] private Button btnCancel = null;
+        [UI] private Button btnConfig = null;
 
-        [UI] private Entry _entryUsername = null;
-        [UI] private Entry _entryPassword = null;
+        [UI] private Entry entryUsername = null;
+        [UI] private Entry entryPassword = null;
         #endregion
 
-        public LoginWindow() : this(new Builder("LoginWindow.ui")) { }
+        private readonly ILogger<LoginWindow> logger;
+
+        public LoginWindow(
+            ILogger<LoginWindow> logger
+        ) : this(new Builder("LoginWindow.ui"), logger) { }
 
         private LoginWindow(
-            Builder builder
+            Builder builder,
+            ILogger<LoginWindow> logger
             ) : base(builder.GetObject("LoginWindow").Handle)
         {
+            // Initialization
             builder.Autoconnect(this);
-            
-            // Window Event bindings
-            Destroyed += (s, e) => Application.Quit();
+            this.logger = logger;
 
-            // UI Event Bindings
-            _entryUsername.Activated += Entry_Username_Activated;
-            _entryPassword.Activated += Entry_Password_Activated;
-            _btnCancel.Clicked += Button_Cancel_Clicked;
-            _btnLogin.Clicked += Button_Login_Clicked;
-            _btnConfig.Clicked += Button_Config_Clicked;
+            BindEventsWindow();
+            BindEventsUi();
+        }
+
+        private void BindEventsWindow()
+        {
+            Destroyed += (s, e) => Application.Quit();
+        }
+
+        private void BindEventsUi()
+        {
+            entryUsername.Activated += Entry_Username_Activated;
+            entryPassword.Activated += Entry_Password_Activated;
+            entryUsername.Focused += (s, e) => entryUsername.SelectRegion(0, -1);
+            entryPassword.Focused += (s, e) => entryPassword.SelectRegion(0, -1);
+            btnCancel.Clicked += Button_Cancel_Clicked;
+            btnLogin.Clicked += Button_Login_Clicked;
+            btnConfig.Clicked += Button_Config_Clicked;
         }
 
         private void Entry_Username_Activated(object sender, EventArgs e)
         {
-            Console.WriteLine("Username Activated");
-            _entryPassword.GrabFocus();
+            entryPassword.GrabFocus();
         }
 
         private void Entry_Password_Activated(object sender, EventArgs e)
@@ -48,8 +63,8 @@ namespace Jupiter
 
         private void Button_Login_Clicked(object sender, EventArgs e)
         {
-            Console.WriteLine("User:" + _entryUsername.Buffer.Text);
-            Console.WriteLine("Pass:" + _entryPassword.Buffer.Text);
+            Console.WriteLine("User:" + entryUsername.Buffer.Text);
+            Console.WriteLine("Pass:" + entryPassword.Buffer.Text);
         }
 
         private void Button_Config_Clicked(object sender, EventArgs e)
